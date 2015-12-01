0d3e25d80e778d07bbc393a649f014577c09e70b
HYEMIBYUN <fc1202@naver.com>
2015-12-01 19:00:52 +0900

    Make it executable on the Smart Phone

---
+++ Proj_HwaBuHe/app/src/main/java/com/example/hyemi/proj_hwabuhe/TrashMainActivity.java
@@ -0,0 +1,17 @@
+package com.example.hyemi.proj_hwabuhe;
+
+        import android.app.Activity;
+        import android.os.Bundle;
+
+/**
+ * Created by juyeon on 2015-11-21.
+ */
+public class TrashMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trashmain);
+
+    }
+}
--- README.md
+++
@@ -1,5 +0,0 @@
-# HwaBuHe
-Human Computer Interaction X SKCC Project 
-
-2015 2nd Semester @ Korea University  
-  w/ Lim Ju Yeon
